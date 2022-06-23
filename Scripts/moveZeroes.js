cb8ece3da0b57791e9b46b99b3e2934f057d5373
shree_sharma_07 <shri.kumar@prepladder.com>
2022-06-23 10:26:53 +0530

    Returing Array on Basis of `Method Args` in MoveZero Solution.

--- Scripts/moveZeroes.js
+++ Scripts/moveZeroes.js
@@ -1,13 +1,39 @@
 // * // Function to Move Zeroes to the End of Array //
-function moveZeroes(nums = []) {
-    return nums.sort((a, b) => {
-        if (b === 0) {
-            return b - a
-        } else {
-            return b
-        }
-    })
+function moveZeroes(nums = [], method = 1) {
+
+    // * Return Array //
+    let return_array = undefined;
+
+    // * // Swtich Case //
+    switch (method) {
+        case 1:
+            return_array = nums.sort((a, b) => {
+                if (b === 0) {
+                    return b - a
+                } else {
+                    return b
+                }
+            });
+            break;
+        case 2:
+            return_array = nums.sort((a, b) => (b > 0 ? 1 : -1))
+            break;
+        case 3:
+            const zeroes = nums.filter((num) => num === 0);
+            const otherNums = nums.filter((num) => num !== 0);
+            return_array = [...otherNums, ...zeroes];
+            break;
+        default:
+            return_array = [{ status: false, message: 'Method Not Allowed!', result: [] }]
+
+
+    }
+
+    // ! // Return Array //
+    return { method, return_array };
 }
 
-console.log(moveZeroes([7, 2, 0, 0, 0, 13, 8, 0, 78, 9, 0]));
-console.log(moveZeroes([0, 78, 9, 0, 13, 8]));
+console.log(moveZeroes([7, 2, 0, 0, 0, 13, 8, 0, 78, 9, 0], 1));
+console.log(moveZeroes([0, 78, 9, 0, 13, 8], 1));
+console.log(moveZeroes([0, 78, 9, 0, 13, 8], 2));
+console.log(moveZeroes([0, 78, 9, 0, 13, 8], 3));
