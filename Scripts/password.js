4486251ea7efc5c32d764470b18b7c5b46895a88
Shree Kumar Sharma <54742998+shreesharma07@users.noreply.github.com>
2022-12-14 14:08:21 +0530

    Update password.js

--- Scripts/password.js
+++ Scripts/password.js
@@ -1,43 +1,49 @@
-// ! // Import Module Specific Function //
-const proc = require('child_process').spawn('pbcopy');
-
 // * // Set of Alphanumeric Characters along with Symbols //
-const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*';
+const CHARACTER_SET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
 
 // @ // Writing Statement to Console //
-process.stdout._write('Enter Your Password Length: ', 'utf8', (err, res) => {
+process.stdout._write("Enter Your Password Length: ", "utf8", (err, res) => {
     try {
-
         // ! // Check for Errors //
         if (err) {
-            throw new Error('Error Occured :', {
-                cause: err
-            })
+            throw new Error("Error Occured :", {
+                cause: err,
+            });
         }
 
         // @ // Reading Number from Console //
-        process.stdin.on('data', data => {
-
+        process.stdin.on("data", (data) => {
             // * // Password Container //
-            let ps = ''
+            let ps = "";
 
             while (ps.length <= parseInt(data)) {
-                ps += alpha.charAt(Math.floor(Math.random() * alpha.length))
+                ps += CHARACTER_SET.charAt(Math.floor(Math.random() * CHARACTER_SET.length));
             }
 
-            proc.stdin.write(ps); proc.stdin.end();
-            console.log(`Your Password is copied to clipboard!`);
+            // * // Getting Os Type //
+            const os_type = require("os").type();
+
+            // ! // Checking OS Type //
+            if (os_type === "Windows_NT") {
+                // ! // Import Module //
+                const clipBoard = require("node-clipboardy");
+                clipBoard.writeSync(ps);
+            } else {
+                // ! // Import Module //
+                const proc = require("child_process").spawn("pbcopy");
+                // * // Copy Token to Clipboard //
+                proc.stdin.write(ps);
+                proc.stdin.end();
+            }
+
+            console.log(`Your Password : ${ps}`);
             process.exit();
-
         });
-
     } catch (error) {
-
         if (error) {
-            throw new Error('Error Occured :', {
-                cause: error
-            })
+            throw new Error("Error Occured :", {
+                cause: error,
+            });
         }
-
     }
-})
+});
